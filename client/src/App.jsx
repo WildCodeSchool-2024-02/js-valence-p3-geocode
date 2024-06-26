641c7e3d55c668061d084999dfc78d6a0ad8a198
kaled guzman <guzman.kaled.pro@gmail.com>
2024-06-26 10:12:38 +0200

    fix test

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,11 @@
+import { Outlet } from "react-router-dom";
 import NavBar from "./components/NavBar";
-import Home from "./pages/Home";
 
 function App() {
   return (
     <div>
       <NavBar />
-      <Home />
+      <Outlet />
     </div>
   );
 }
